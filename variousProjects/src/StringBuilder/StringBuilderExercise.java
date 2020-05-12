46b7766d43b871b7bcf740579be08effa9a691df
zarrar <zarrarmalik81@gmail.com>
2020-05-12 10:14:56 +0100

    Add custom exception

--- variousProjects/src/StringBuilder/StringBuilderExercise.java
+++ variousProjects/src/StringBuilder/StringBuilderExercise.java
@@ -1,20 +1,24 @@
 package StringBuilder;
 
 public class StringBuilderExercise {
-	public String hello(String name) {
-		StringBuilder stre = new StringBuilder();
-		String greetings = "Hello";
-		StringBuilder friendName = stre.append(greetings).append(",").append(" ").append(name).append("!");
-		String greetingMessage = friendName.toString();
-		return greetingMessage;
+    public String hello(String name) throws nameNotFoundException {
+        String greetingMessage = "";
+        StringBuilder stre = new StringBuilder();
+        if (name.equalsIgnoreCase("John")) {
+            String greetings = "Hello";
+            StringBuilder friendName = stre.append(greetings).append(",").append(" ").append(name)
+                    .append("!");
+            greetingMessage = friendName.toString();
 
-	}
+        } else {
+            throw new nameNotFoundException("Entered name is not John, please enter John");
+        }
+        return greetingMessage;
+    }
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws nameNotFoundException {
 		StringBuilderExercise str = new StringBuilderExercise();
 		str.hello("John");
-		System.out.println(str.hello("John"));
-
+		System.out.println(str.hello("Johnyy"));
 	}
-
 }
